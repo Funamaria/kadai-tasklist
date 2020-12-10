6d0cb06d29d87322b582ef16d4fe6a6dab2287a6
Funamaria <nfunamaria.stella@gmail.com>
2020-12-10 02:18:53 +0000

    add_status_to_column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -47,9 +47,11 @@ class TasksController extends Controller
     {
         //
         $request->validate([
+            'status' => 'required|max:10',
             'content' => 'required|max:255',
             ]);
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
@@ -99,10 +101,11 @@ class TasksController extends Controller
     {
         //
         $request->validate([
+            'status' => 'required|max:10',
             'content' => 'required|max:255',
             ]);
         $task = Task::findOrFail($id);
-        
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -8,6 +8,7 @@
         <thread>
             <tr>
                 <th>id</th>
+                <th>ステータス</th>
                 <th>タスク</th>
             </tr>
         </thread>
@@ -15,6 +16,7 @@
             @foreach ($tasks as $task)
             <tr>
                 <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
+                <td>{{ $task->status }}</td>
                 <td>{{ $task->content }}</td>
             </tr>
             @endforeach
